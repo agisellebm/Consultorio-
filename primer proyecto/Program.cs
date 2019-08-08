35080a0c4f098647bc4800c0c919f854b475a215
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-08 07:41:54 -0700

    Agregar archivos de proyecto.

---
+++ primer proyecto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_proyecto
+{
+    class Paciente
+    {
+        //Atributos (variables)
+        string nombre;
+        string direccion;
+        string telefono;
+        DateTime fechaDeNacimiento;
+        string enfermedadesCronicas;
+
+        //Metodos (funciones)
+    }
+}
